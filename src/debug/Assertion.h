93cb89e4d77659da53cbdc5f4c44d6a3d6f0eea2
Daniel Laplana Gimeno <daniel.laplana.gimeno@gmail.com>
2025-02-03 00:31:30 +0100

    Updated Readme and debug log

--- src/debug/Assertion.h
+++ src/debug/Assertion.h
@@ -1,7 +1,6 @@
 //
 // Created by Daniel Laplana Gimeno on 5/12/24.
 //
-#include <stdio.h>
 #include <iostream>
 
 #ifndef AURAGL_ASSERTION_H
@@ -11,12 +10,12 @@
 
 #ifdef COMETA_DEBUG
     #define COMETA_ASSERT(x) Assertion::Assert(x)
-    #define COMETA_ERROR(x) Assertion::Error(x)
     #define COMETA_WARNING(x) Assertion::Warning(x)
+    #define COMETA_ERROR(x) Assertion::Error(x)
 #else
     #define COMETA_ASSERT(x) ((void)0)
-    #define COMETA_ERROR(x) ((void)0)
     #define COMETA_WARNING(x) ((void)0)
+    #define COMETA_ERROR(x) ((void)0)
 #endif
 
 enum AssertLevel{
