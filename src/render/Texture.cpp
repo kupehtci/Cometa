8d30e16e0598e9102fa29a97f4561379177e64f5
Daniel Laplana Gimeno <daniel.laplana.gimeno@gmail.com>
2025-02-19 19:09:49 +0100

    Texture loading implemented

--- src/render/Texture.cpp
+++ src/render/Texture.cpp
@@ -1,16 +1,63 @@
 #include "Texture.h"
 
+
 Texture::Texture() {
+	_uid = 0;
+	_uid = 0;
+	_path = "";
+	_width = _height = 0;
+	_channels = 0;
 
 }
 
 Texture::Texture(std::string filePath){
 
+	_uid = 0;
+	_path = filePath; 
+	_width = _height = 0; 
+	_channels = 0; 
+	
+	// Load image using STBI
+	int width, height, nrChannels;
+	const char* texture0Path = filePath.c_str();
+	stbi_uc* data = nullptr; 
+
+	data = stbi_load(texture0Path, &width, &height, &nrChannels, 0);
+
+	// Check that channels in RGB are 3 or 4
+	if (nrChannels != 3 && nrChannels != 4) {
+		COMETA_ERROR("Texture format not supported"); 
+	}
+
+	if (data != nullptr) {
+		glGenTextures(1, &_uid);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glBindTexture(GL_TEXTURE_2D, _uid);
+
+		GLenum format = nrChannels == 4 ? GL_RGBA : GL_RGB; 
+		GLenum internalFormat = nrChannels == 4 ? GL_RGBA8 : GL_RGB8; 
+
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, _width, _height, 0, format, GL_UNSIGNED_BYTE, data);
+		// glTextureSubImage2D
+
+		stbi_image_free(data);
+	}
+	else {
+		COMETA_ERROR("Unable to load texture"); 
+		return; 
+	}
+
 }
 
 
-void Texture::Bind() {
-
+void Texture::Bind(GLenum textureNumber) {
+	glActiveTexture(textureNumber);
+	//glBindTexture(GL_TEXTURE_2D, _uid);
 }
 
 bool const Texture::operator==(const Texture& other) {
--- src/render/Texture.h
+++ src/render/Texture.h
@@ -5,7 +5,15 @@
 #include <string>
 
 // #include "Window.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <glm.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "../debug/Assertion.h"
 
 class Texture {
 
@@ -13,14 +21,15 @@ private:
 	uint32_t _uid;			// Unique ID of the loaded texture. In OpenGL this uid references the texture
 	uint32_t _width;		// Width of the loaded texture
 	uint32_t _height;		// Height of the loaded texture
+	uint8_t _channels;		
 	std::string _path;		// Store the path of the loaded texture
 
 public: 
-	Texture() = default; 
+	Texture(); 
 
 	Texture(std::string filePath); 
 
-	void Bind(); 
+	void Bind(GLenum textureNumber);
 
 	// Operator overload
 	
@@ -34,6 +43,7 @@ public:
 	inline uint32_t GetHeight() { return _height;  }
 	inline glm::vec2 GetSize() { return glm::vec2(_width, _height); }
 	inline std::string GetPath() { return _path; }
+	inline bool IsLoaded() { return _uid != 0;  }
 };
 
 #endif
