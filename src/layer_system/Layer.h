33dec6e1fb208aaf3f4ffdc68aa23ffe2fa32a73
Daniel Laplana Gimeno <daniel.laplana.gimeno@gmail.com>
2025-02-28 22:30:58 +0100

    Layers initial implementation

---
+++ src/layer_system/Layer.h
@@ -0,0 +1,20 @@
+#ifndef COMETA_LAYER_H
+#define COMETA_LAYER_H
+
+#include "Event.h"
+
+class Layer{
+
+private: 
+
+
+public:
+    // Layer();
+    ~Layer();
+    virtual void Init() = 0;
+    virtual void Update() = 0;
+    virtual void Close() = 0;
+    virtual void HandleEvent(Event& event) = 0;
+}; 
+
+#endif
