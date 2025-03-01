929c7f01175bae96c5038b122fc240c41b1aa618
Daniel Laplana Gimeno <daniel.laplana.gimeno@gmail.com>
2025-03-01 13:01:28 +0100

    Instances begin implementation

---
+++ src/world/Instance.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel Laplana Gimeno on 1/3/25.
+//
+
+#include "Instance.h"
---
+++ src/world/Instance.h
@@ -0,0 +1,14 @@
+//
+// Created by Daniel Laplana Gimeno on 1/3/25.
+//
+
+#ifndef AURAGL_INSTANCE_H
+#define AURAGL_INSTANCE_H
+
+
+class Instance {
+
+};
+
+
+#endif //AURAGL_INSTANCE_H
