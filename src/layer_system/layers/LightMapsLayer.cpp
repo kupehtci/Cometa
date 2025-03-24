48df29ec6784ceacbdb49c5add5490e0fbc1bd24
Daniel Laplana Gimeno <daniel.laplana.gimeno@gmail.com>
2025-03-24 17:50:54 +0100

    Initial commit

---
+++ src/layer_system/layers/LightMapsLayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Daniel Laplana Gimeno on 24/3/25.
+//
+
+#include "LightMapsLayer.h"
---
+++ src/layer_system/layers/LightMapsLayer.h
@@ -0,0 +1,14 @@
+//
+// Created by Daniel Laplana Gimeno on 24/3/25.
+//
+
+#ifndef AURAGL_LIGHTMAPSLAYER_H
+#define AURAGL_LIGHTMAPSLAYER_H
+
+
+class LightMapsLayer {
+
+};
+
+
+#endif //AURAGL_LIGHTMAPSLAYER_H
